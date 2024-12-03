f13b5e78af5746ba9e79107e5b949c61e21594cc
Bingkui Tong <tbbbk@qq.com>
2024-12-03 14:30:14 +0800

    Add flag counter

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// 显示模态框
 function showModal() {
     document.getElementById('cvModal').style.display = 'block';
     document.getElementById('cvModalOverlay').style.display = 'block';
 }
 
-// 隐藏模态框
 function hideModal() {
     document.getElementById('cvModal').style.display = 'none';
     document.getElementById('cvModalOverlay').style.display = 'none';
+}
+
+function playSound() {
+    var audio = document.getElementById('sound');
+    audio.play();
 }
