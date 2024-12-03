a0b50cecf6799a4104a60118ddf4053190a44f0f
Bingkui Tong <tbbbk@qq.com>
2024-12-03 14:00:16 +0800

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+// 显示模态框
+function showModal() {
+    document.getElementById('cvModal').style.display = 'block';
+    document.getElementById('cvModalOverlay').style.display = 'block';
+}
+
+// 隐藏模态框
+function hideModal() {
+    document.getElementById('cvModal').style.display = 'none';
+    document.getElementById('cvModalOverlay').style.display = 'none';
+}
